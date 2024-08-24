36b28b65343eefae53008a601050c52bc5cc75c6
rahul-toddleapp <rahulsingh@toddleapp.com>
2024-08-24 18:23:07 +0530

    chore : update header

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export function Header() {
   return (
     <header>
-      Lerna is the original monorepo tool! hello there get new there v234
+      Lerna is the original monorepo tool! hello there get new there v234cds
     </header>
   );
 }
